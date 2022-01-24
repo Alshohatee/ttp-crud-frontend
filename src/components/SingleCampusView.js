badf63a76fa631975f1ebc43b3c2774e8fcd710b
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2022-01-23 22:36:37 -0500

    Cleaned up SingleCampusView Component code

--- src/components/SingleCampusView.js
+++ src/components/SingleCampusView.js
@@ -1,16 +1,8 @@
-/* eslint-disable jsx-a11y/alt-text */
 import NavBar from './NavBar'
 import Axios from "axios"
 import React, { useEffect, useState } from "react";
 import CampusCard from './CampusCard'
 
-// ○	[   ] see details about a single campus, including enrolled students (if any)
-// ○	[   ] see an informative message if no students are enrolled at that campus
-// ○	[   ] navigate to any student’s single student view 
-// ○	[   ] delete the campus (and elegantly handle associated students)
-// ○	[   ] edit campus information (including adding/removing students)
-//     ■	[   ] with a validated form displaying real-time error messages
-//     ■	[   ] which redirects back to the single campus view
 
 export default function SingleCampusView() {
       const [campusesInfo, setCampusInfo] = useState([])
@@ -20,47 +12,30 @@ export default function SingleCampusView() {
         const getId = window.location.href.split("/");
         const id  = getId[getId.length -1]
 
-    async function fectchData(){
+    async function fetchData(){
       const campusInfo = await Axios.get(`http://localhost:8080/api/campuses/${id}`)
-      console.log("this", campusInfo)
       setCampusInfo(campusInfo.data)
-      alert("help")
     }
-    fectchData()
+    fetchData().then(() => console.log("Data Fetched"))
      
   },[]);
 
   function handleOnClick(){
-
-    // DELETE request using axios with error handling
-  Axios.delete(`http://localhost:8080/api/campuses/${campusesInfo.id}`)
+    Axios.delete(`http://localhost:8080/api/campuses/${campusesInfo.id}`)
         .then(response => setIsDelete(true))
-        
+
         .catch(error => {
           setIsDelete(false)
             console.error('There was an error!', error);
         })
-setCampusInfo([])
-      }
-      
-
+    setCampusInfo([])
+  }
 
     return (
         <div>
-        <NavBar />
-       {/* <img  src={campusesInfo.imageUrl} />
-        <p> {campusesInfo.name}</p>
-        <p> {campusesInfo.address}</p>
-        <p> {campusesInfo.description}</p>
-        <button onClick={()=> handleOnClick()}>
-          Delete
-        </button> */}
-
-      <CampusCard  key={campusesInfo.id}  campus={[campusesInfo,true,handleOnClick ] } />
-    { isDelete ? <h1> {campusesInfo.name} Deleted </h1> : null}
-
-{/* studentsAtCampus will be render here  */}
-            
+            <NavBar />
+            <CampusCard  key={campusesInfo.id}  campus={[ campusesInfo,true,handleOnClick ]} />
+            { isDelete ? <h1> {campusesInfo.name} Deleted </h1> : null}
         </div>
     )
 }
